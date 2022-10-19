d9ff88e55393a13de2885c885c3981ffa75705c0
Asifa Ahmad <97321673+Lushiva@users.noreply.github.com>
2022-10-19 13:54:18 +0700

    Project Tugas RPL Semester 3
    Ini punyanya sifa.

---
+++ LowVibe/app/src/main/java/com/example/lowvibe/MitologiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lowvibe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MitologiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mitologi)
+    }
+}
